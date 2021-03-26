d90d20e8b9c20446c24354a5560a0b81824dcb8e
robbridges <robbridges@users.noreply.github.com>
2021-03-25 21:42:01 -0700

    Correctly imported CSS

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -3,36 +3,56 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 /*
-Main route that lists all of the courses, we are correctly pushing the courses to and allowing CORS, 
-we just need to find a way to display correctly, as the data is good the presentation of it is not.
-
-*/
+  Set up the state of our courses to blank array
+ */
 export default class Courses extends Component {
   state = {
+    // populate array with fetched data
     courses: [],
   };
 
-  // component first mounts (or on reload), make axios call to API to retrieve the list of courses in the database, we just need to correctly render them. 
+  // once component mounted load the courses into the array to display
   componentDidMount() {
     axios
       .get(`http://localhost:5000/api/courses`)
       .then((data) => {
         this.setState({
           courses: data.data,
-          
+          user: data.data.User,
         });
       })
       .catch((error) => {
         console.log('Error fetching and parsing data', error);
-
+        
       });
   }
 
   
 
-  
+  //push the populated data into the app
   render() {
     const results = this.state.courses;
-    return;
+    let courses = results.map((course) => (
+      <React.Fragment key={course.id}>
+        <div className='grid-33'>
+          <Link
+            className='course--module course--link'
+            to={`/courses/${course.id}`}
+          >
+            <h4 className='course--label'>Course</h4>
+            <h3 className='course--title'>{course.title}</h3>
+          </Link>
+        </div>
+      </React.Fragment>
+    ));
+
+    return(
+      <div className='bounds'>
+        {courses}
+      </div>
+    )
   }
 }
+
+
+
