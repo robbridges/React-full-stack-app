abd5546ffb3d250532effe7207960a4ef6fd0668
robbridges <robbridges@users.noreply.github.com>
2021-03-29 10:36:22 -0700

    Update Error.js

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -4,7 +4,7 @@ function NotFound( ) {
   return (
     <div className="wrap">
       <h1> Not found</h1>
-      <p> Sorry we couldn't find the page that you're looking for</p>
+      <p> Sorry we couldn't find the page that you're looking for.</p>
 
     </div>
   )
