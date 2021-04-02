0ec93c42528757f330136f6edd1af88c1a858c39
robbridges <robbridges@users.noreply.github.com>
2021-04-02 12:49:58 -0700

    final updates

--- client/src/App.js
+++ client/src/App.js
@@ -50,7 +50,7 @@ const App = () => (
           <Route path="/signout" component={UserSignOutWithContext} />
           <Route path ="/error" component={UnhandledError} />
           <Route path ="/forbidden" component={Forbidden} />
-          <Route path ="/notFound" component={NotFound} />
+          <Route path ="/notfound" component={NotFound} />
           <Route component={NotFound} />
           
           
